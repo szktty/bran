472e02323869079f87f90a66ba39c479d86c4183
SUZUKI Tetsuya <tetsuya.suzuki@gmail.com>
2014-11-26 21:54:12 +0900

    restruct Erlang library directory

---
+++ liberl/src/bran_lib_pervasives.erl
@@ -0,0 +1,5 @@
+-module(bran_lib_pervasives).
+
+-export([print_string/1]).
+
+print_string(S) -> io:format("~p", [S]).
---
+++ liberl/test/bran_tests.erl
@@ -0,0 +1,3 @@
+-module(bran_tests).
+-include_lib("eunit/include/eunit.hrl").
+
