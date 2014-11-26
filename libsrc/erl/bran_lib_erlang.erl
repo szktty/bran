790bcaee3ba43f7e799a88c8586ff4bf09fc237b
SUZUKI Tetsuya <tetsuya.suzuki@gmail.com>
2014-11-26 22:00:45 +0900

    restruct libsrc

--- libsrc/erl/bran_lib_erlang.erl
+++
@@ -1,9 +0,0 @@
--module(bran_lib_erlang).
-
--export([eval/1]).
-
-eval(S)->
-    {ok, Toks, _} = erl_scan:string(S),
-    {ok, [Exp]} = erl_parse:parse_exprs(Toks),
-    {value, Ret, _} = erl_eval:expr(Exp, erl_eval:bindings(erl_eval:new_bindings())),
-    hd(io_lib:fwrite("~p", [Ret])).
--- libsrc/erl/bran_lib_pervasives.erl
+++
@@ -1,5 +0,0 @@
--module(bran_lib_pervasives).
-
--export([print_string/1]).
-
-print_string(S) -> io:format("~p", [S]).
