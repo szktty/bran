6f53cfcb11754ae25b2be0b9e58851f5b19f158f
SUZUKI Tetsuya <tetsuya.suzuki@gmail.com>
2014-11-27 14:20:00 +0900

    add development build script

---
+++ devbuild.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+#opam switch 4.02.1
+#eval `opam config env`
+rm -rf bran bran-develop.tar.gz
+git clone git@github.com:szktty/bran.git
+cd bran
+omake --project
+omake --project test
+rm -rf .git .gitignore devbuild.sh circle.yml circle src test OMake* lib/OMake* liberl/OMake*
+cd ..
+tar czf bran-develop.tar.gz bran
