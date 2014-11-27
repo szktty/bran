f28428825c5eac134a18cfa18fa6a2bc7e22be7d
SUZUKI Tetsuya <tetsuya.suzuki@gmail.com>
2014-11-27 22:14:21 +0900

    fix download URL and removing unused files

--- devbuild.sh
+++ devbuild.sh
@@ -3,10 +3,8 @@
 #opam switch 4.02.1
 #eval `opam config env`
 rm -rf bran bran-develop.tar.gz
-git clone git@github.com:szktty/bran.git
+git clone https://github.com/szktty/bran.git
 cd bran
 omake --project
-omake --project test
-rm -rf .git .gitignore devbuild.sh circle.yml circle src test OMake* lib/OMake* liberl/OMake*
+omake --project test && rm -rf .git .gitignore devbuild.sh circle.yml circle src test OMake* lib/OMake* liberl/OMake* && tar czf ../bran-develop.tar.gz .
 cd ..
-tar czf bran-develop.tar.gz bran
