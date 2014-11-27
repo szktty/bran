af15f4f35f8ac83b7635c32f6992a71043923910
SUZUKI Tetsuya <tetsuya.suzuki@gmail.com>
2014-11-27 23:06:54 +0900

    add os name and cpu name to zip file

--- devbuild.sh
+++ devbuild.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
+ZIP=bran-develop-`uname -s`-`uname -m`.tar.gz
+
 #opam switch 4.02.1
 #eval `opam config env`
-rm -rf bran bran-develop.tar.gz
+rm -rf bran $ZIP
 git clone https://github.com/szktty/bran.git
 cd bran
 omake --project
-omake --project test && rm -rf .git .gitignore devbuild.sh circle.yml circle src test OMake* lib/OMake* liberl/OMake* && tar czf ../bran-develop.tar.gz .
+omake --project test && rm -rf .git .gitignore devbuild.sh circle.yml circle src test OMake* lib/OMake* liberl/OMake* && tar czf ../$ZIP .
 cd ..
