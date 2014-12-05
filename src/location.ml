e93c500429a3bce1220369eb58abb99dc04b9cfd
SUZUKI Tetsuya <tetsuya.suzuki@gmail.com>
2014-12-05 23:37:39 +0900

    change to_string format

--- src/location.ml
+++ src/location.ml
@@ -33,6 +33,6 @@ let contains_offset loc offset =
   loc.start.offset <= offset && offset < loc.end_.offset
 
 let to_string loc =
-  Printf.sprintf "%d:%d(%d)-%d:%d(%d)"
+  Printf.sprintf "%d:%d:%d-%d:%d:%d"
     loc.start.line loc.start.col loc.start.offset
     loc.end_.line loc.end_.col loc.end_.offset
