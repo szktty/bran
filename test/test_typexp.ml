94595a6ba1d1e95372f018beff59a4a195b5e106
SUZUKI Tetsuya <tetsuya.suzuki@gmail.com>
2014-12-07 03:38:00 +0900

    add tests

--- test/test_typexp.ml
+++ test/test_typexp.ml
@@ -20,7 +20,29 @@ let test_parsing file expect test_ctx =
   assert_equal expect res.stdout
 
 let parsing_files = [
+  (* primitive types *)
   ("unit", "unit.bri", "unit");
+  ("bool", "bool.bri", "bool");
+  ("char", "char.bri", "char");
+  ("int", "int.bri", "int");
+  ("float", "float.bri", "float");
+  ("atom", "atom.bri", "atom");
+  ("string", "string.bri", "string");
+  ("bitstring", "bitstring.bri", "bitstring");
+  ("binary", "binary.bri", "binary");
+
+  (* type functions *)
+  ("list", "list.bri", "string list");
+
+  (* functions *)
+  ("fun_of_unit_unit", "fun_of_unit_unit.bri", "unit -> unit");
+  ("fun_of_int_float_string", "fun_of_int_float_string.bri",
+   "int -> float -> string");
+  ("fun_of_fun", "fun_of_fun.bri", "(char -> bool) -> string -> string list");
+
+  (* polymorphic types *)
+  ("poly_list", "poly_list.bri", "'a list");
+  ("poly_fun", "poly_fun.bri", "('a -> 'b) -> 'b list -> 'b");
 ]
 
 let suite =
