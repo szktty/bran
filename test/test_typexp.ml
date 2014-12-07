9754787edab0b77c311b328885f46e092b3e79a5
SUZUKI Tetsuya <tetsuya.suzuki@gmail.com>
2014-12-07 16:34:44 +0900

    rename test file names to avoid conflict with builtin modules

--- test/test_typexp.ml
+++ test/test_typexp.ml
@@ -21,29 +21,29 @@ let test_parsing file expect test_ctx =
 
 let parsing_files = [
   (* primitive types *)
-  ("unit", "unit.bri", "unit");
-  ("bool", "bool.bri", "bool");
-  ("char", "char.bri", "char");
-  ("int", "int.bri", "int");
-  ("float", "float.bri", "float");
-  ("atom", "atom.bri", "atom");
-  ("string", "string.bri", "string");
-  ("bitstring", "bitstring.bri", "bitstring");
-  ("binary", "binary.bri", "binary");
-  ("tuple", "tuple.bri", "(int * int)");
+  ("unit", "test_unit.bri", "unit");
+  ("bool", "test_bool.bri", "bool");
+  ("char", "test_char.bri", "char");
+  ("int", "test_int.bri", "int");
+  ("float", "test_float.bri", "float");
+  ("atom", "test_atom.bri", "atom");
+  ("string", "test_string.bri", "string");
+  ("bitstring", "test_bitstring.bri", "bitstring");
+  ("binary", "test_binary.bri", "binary");
+  ("tuple", "test_tuple.bri", "(int * int)");
 
   (* type functions *)
-  ("list", "list.bri", "string list");
+  ("list", "test_list.bri", "string list");
 
   (* functions *)
-  ("fun_of_unit_unit", "fun_of_unit_unit.bri", "unit -> unit");
-  ("fun_of_int_float_string", "fun_of_int_float_string.bri",
+  ("fun_of_unit_unit", "test_fun_of_unit_unit.bri", "unit -> unit");
+  ("fun_of_int_float_string", "test_fun_of_int_float_string.bri",
    "int -> float -> string");
-  ("fun_of_fun", "fun_of_fun.bri", "(char -> bool) -> string -> string list");
+  ("fun_of_fun", "test_fun_of_fun.bri", "(char -> bool) -> string -> string list");
 
   (* polymorphic types *)
-  ("poly_list", "poly_list.bri", "'a list");
-  ("poly_fun", "poly_fun.bri", "('a -> 'b) -> 'b list -> 'b");
+  ("poly_list", "test_poly_list.bri", "'a list");
+  ("poly_fun", "test_poly_fun.bri", "('a -> 'b) -> 'b list -> 'b");
 ]
 
 let suite =
