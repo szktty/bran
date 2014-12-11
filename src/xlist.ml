ed4089120fbe48ccbe07126f114cee240e9f66b2
SUZUKI Tetsuya <tetsuya.suzuki@gmail.com>
2014-12-12 03:00:52 +0900

    add utilify functions

--- src/xlist.ml
+++ src/xlist.ml
@@ -8,3 +8,21 @@ let for_all2 pred xs ys =
     List.for_all2 pred xs ys
   with 
     Invalid_argument _ -> false
+
+let rec inject f1 f2 = function
+  | [] -> ()
+  | [e] -> f2 e
+  | e :: es ->
+    f2 e;
+    f1 ();
+    inject f1 f2 es
+
+let rec inject2 f1 f2 xs ys =
+  match xs, ys with
+  | [], [] -> ()
+  | [], _ | _, [] -> raise (Invalid_argument "inject2")
+  | [x], [y] -> f2 x y
+  | x :: xs, y :: ys ->
+    f2 x y;
+    f1 ();
+    inject2 f1 f2 xs ys
--- src/xlist.mli
+++ src/xlist.mli
@@ -1,2 +1,4 @@
 val init : 'a list -> 'a list
 val for_all2 : ('a -> 'b -> bool) -> 'a list -> 'b list -> bool
+val inject : (unit -> unit) -> ('a -> unit) -> 'a list -> unit
+val inject2 : (unit -> unit) -> ('a -> 'b -> unit) -> 'a list -> 'b list -> unit
