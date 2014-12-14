fcbef0243355db057dee4b09139707e5ef6d9300
SUZUKI Tetsuya <tetsuya.suzuki@gmail.com>
2014-12-15 01:34:24 +0900

    impl resolve patterns

--- src/naming.ml
+++ src/naming.ml
@@ -34,13 +34,14 @@ let find_val env loc path =
     | Some t -> `Module (Binding.add (Module.path m) name, t)
 
 let rec resolve env mx { loc = loc; desc = (e, t) } =
-  (* TODO *)
   Log.debug "# Naming.resolve : %s\n" (Ast.string_of_expr e);
+  let f = resolve env mx in
+  let map = List.map f in
   let e', t' =
     match e with
     | Unit | Bool _ | Int _ | Float _ | Char _ | String _ | Atom _ | Bitstring _ -> e, t
     | Match (e1, ptns) ->
-      Match (resolve env mx e1, List.map (fun (p, e) -> p, resolve env mx e) ptns), t
+      Match (f e1, List.map (fun (p, e) -> p, f e) ptns), t
     | Var (`Unbound x) ->
       begin match find_val env loc x with
         | `Not_found name -> raise (Unbound_value_error (loc, name, []))
@@ -52,40 +53,38 @@ let rec resolve env mx { loc = loc; desc = (e, t) } =
       begin match find_val env loc x with
         | `Not_found name -> raise (Unbound_constr_error (loc, name, []))
         | `Local (name, t') ->
-          Constr (Binding.add mx name, List.map (resolve env mx) es), t'
-        | `Module (x', t') -> Constr (x', List.map (resolve env mx) es), t'
+          Constr (Binding.add mx name, map es), t'
+        | `Module (x', t') -> Constr (x', map es), t'
       end
-        (*
-  | Record of (Id.t * t) list
-  | Field of t * Id.t
-  | List of t list
-  | Tuple of t list
-  | Array of t list
-  | Not e | 
-  | And of t * t
-  | Or of t * t
-  | Neg of t
-  | Add of t * t
-  | Sub of t * t
-  | Mul of t * t
-  | Div of t * t
-  | Eq of t * t
-  | LE of t * t
-  | If of t * t * t
-  | Match of t * (pattern * t) list
-  | LetVar of (Id.t * Type_t.t) * t * t
-  | Var of Binding.t ref
-  | Concat of t * t
-  | Constr of Binding.t ref * t list
-  | LetRec of fundef * t
-  | App of t * t list
-  | Get of t * t
-  | Put of t * t * t
-  | Perform of t
-  | Bind of (Id.t * Type_t.t) * t
-  | Return of t
-                                                                                     *)
-    | _ -> e, t
+    | List es -> List (map es), t
+    | Tuple es -> Tuple (map es), t
+    | Array es -> Array (map es), t
+    | Not e -> Not (f e), t
+    | Neg e -> Neg (f e), t
+    | And (e1, e2) -> And (f e1, f e2), t
+    | Or (e1, e2) -> Or (f e1, f e2), t
+    | Add (e1, e2) -> Add (f e1, f e2), t
+    | Sub (e1, e2) -> Sub (f e1, f e2), t
+    | Mul (e1, e2) -> Mul (f e1, f e2), t
+    | Div (e1, e2) -> Div (f e1, f e2), t
+    | Eq (e1, e2) -> Eq (f e1, f e2), t
+    | LE (e1, e2) -> LE (f e1, f e2), t
+    | Concat (e1, e2) -> Concat (f e1, f e2), t
+    | If (e1, e2, e3) -> If (f e1, f e2, f e3), t
+    | Get (e1, e2) -> Get (f e1, f e2), t
+    | Put (e1, e2, e3) -> Put (f e1, f e2, f e3), t
+    | Record xes -> Record (List.map (fun (x, e) -> (x, f e)) xes), t
+    | Field (e, x) -> Field (f e, x), t
+    | LetVar ((x, t), e1, e2) ->
+      let env' = Env.add_var env x t in
+      LetVar ((x, t), f e1, resolve env' mx e2), t
+    | LetRec ({ name = xt; args = yts; body = e1 }, e2) ->
+      let env' = Env.add_vars env yts in
+      LetRec ({ name = xt; args = yts; body = resolve env' mx e1 }, f e2), t
+    | App (e, es) -> App (f e, map es), t
+    | Perform e -> Perform (f e), t
+    | Bind (xt, e) -> Bind (xt, f e), t
+    | Return e -> Return (f e), t
   in
   create loc (e', t')
 
@@ -100,7 +99,6 @@ let resolve_def env mx def =
   | _ -> assert false
 
 let f mx defs = 
-(*val fold : (Env.t * def list -> def -> def list) -> def list -> Env.t -> def list*)
   Ast.fold
     (fun (env, defs) def -> resolve_def env mx def :: defs)
     defs (Sig.create_env ())
